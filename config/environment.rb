de9717dd989cfa26367d75220db9c832098fde9c
Robert Bjarnason <robert.bjarnason@gmail.com>
2009-01-14 00:40:49 +0000

    basic layout fixes

--- config/environment.rb
+++ config/environment.rb
@@ -26,7 +26,7 @@ RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.action_controller.relative_url_root = "/dev_1" 
+ # config.action_controller.relative_url_root = "/dev_1" 
   
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
