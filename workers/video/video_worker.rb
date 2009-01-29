959263ebbc14a43d051fe0c689164ebabb02024c
Robert Bjarnason <robert.bjarnason@gmail.com>
2009-01-29 02:05:59 +0000

    more work on video

--- workers/video/video_worker.rb
+++ workers/video/video_worker.rb
@@ -9,11 +9,11 @@ ENV['RAILS_ENV'] = worker_config['rails_env']
 
 options = {
     :app_name   => "video_worker_"+ENV['RAILS_ENV'],
-    :dir_mode   => :system,
+    :dir_mode   => :script,
     :backtrace  => true,
     :monitor    => true,
     :log_output => true,
-    :script     => ENV['RAILS_ENV'] == "development" ? "/home/robert/work/ContentStoreDevelopment/lib/video_worker/video_worker_daemon.rb" : "/home/robert/work/ContentStoreProduction/lib/video_worker/video_worker_daemon.rb" 
+    :script     => "video_worker_daemon.rb" 
   }
 
 Daemons.run(File.join(File.dirname(__FILE__), 'video_worker_daemon.rb'), options)
