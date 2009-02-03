57f720da40fd9c249463689e4ff8995193b0ad0a
Robert Bjarnason <robert.bjarnason@gmail.com>
2009-02-03 19:46:37 +0000

    break on broken wmvs

--- workers/video/utils/shell.rb
+++ workers/video/utils/shell.rb
@@ -11,12 +11,18 @@ class Shell
     "%02d:%02d:%02d.%06d" % [t.hour, t.min, t.sec, t.usec]
   end
 
-  def execute(command)
+  def execute(command, breakon=nil)
     @logger.info("Execute: #{command}")
     IO.popen(command) do |pipe|
       pipe.each("\r") do |line|
         @logger.debug(line)
         $defout.flush
+        if breakon and line.index(breakon)
+          @logger.error("Breaking on: #{breakon}")
+          $defout.flush
+          raise MediaFormatException
+          break
+        end
       end
     end
     raise MediaFormatException if $?.exitstatus != 0
