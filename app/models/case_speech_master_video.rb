7b40fe193d3e9534440c22c098258a162f543f06
Robert Bjarnason <robert.bjarnason@gmail.com>
2009-02-03 03:41:54 +0000

    sort case videos by start_offset

--- app/models/case_speech_master_video.rb
+++ app/models/case_speech_master_video.rb
@@ -2,7 +2,7 @@ class CaseSpeechMasterVideo < ActiveRecord::Base
   has_many :case_speech_video
   
   has_many :case_speech_videos, 
-                          :order => "case_speech_videos.created_at DESC" do
+                          :order => "case_speech_videos.start_offset ASC" do
     def get_one_to_process
       find :first, :conditions => "case_speech_videos.published = 0 AND case_speech_videos.in_processing = 0 AND case_speech_videos.has_checked_duration = 1", :lock => true
     end
