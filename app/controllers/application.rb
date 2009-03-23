ab64a487a030754f66047aa11ce7ce2d59db622b
rbjarnason <RVB@.(none)>
2009-03-23 00:43:35 +0000

    protect from forgery not working

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -26,7 +26,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '216851400334e64c3cf4dcd55b6527cf'
+  #protect_from_forgery # :secret => '216851400334e64c3cf4dcd55b6527cf'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
